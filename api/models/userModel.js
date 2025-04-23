1adf6d1c912bfbc023f7a96444fb4ea00397d58a
Vaisakhhhhh <vaisakh4693@gmail.com>
2025-04-23 14:47:32 +0530

    update header component and make profile private

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://cdn.pixabay.com/photo/2019/08/11/18/59/icon-4399701_640.png",
     }
 }, { timestamps: true });
 
