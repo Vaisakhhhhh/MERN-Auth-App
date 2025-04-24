f795df9333879cdda8348c9754c1074f176b3cc9
Vaisakhhhhh <vaisakh4693@gmail.com>
2025-04-24 16:13:37 +0530

    add admin dashboard to private route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import SignIn from "./pages/user/SignIn";
 import SignUp from "./pages/user/SignUp";
 import Profile from "./pages/user/Profile";
 import Header from "./components/Header";
-import PrivateRoute from "./components/PrivateRoute";
+import PrivateRoute, { AdminPrivateRoute } from "./components/PrivateRoute";
 import Login from "./pages/admin/Login";
 import Dashboard from "./pages/admin/Dashboard";
 import { useLocation } from "react-router-dom";
@@ -32,7 +32,9 @@ const Layout = () => {
 
         {/* Admin Routes */}
         <Route path="/admin-login" element={<Login />} />
-        <Route path="/admin-dashboard" element={<Dashboard />} />
+        <Route element={<AdminPrivateRoute />}>
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+        </Route>
       </Routes>
     </>
   );
--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -6,4 +6,9 @@ const PrivateRoute = () => {
   return currentUser ? <Outlet/> : <Navigate to='/sign-in' />
 }
 
+export const AdminPrivateRoute = () => {
+  const { currentAdmin } = useSelector((state) => state.admin);
+return currentAdmin ? <Outlet/> : <Navigate to='/admin-login' />
+}
+
 export default PrivateRoute
